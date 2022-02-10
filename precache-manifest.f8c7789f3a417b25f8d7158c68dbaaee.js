61fc723331f2df4d9dbdbb6dc1179d2e57c24c67
Derek Schmitt <derek@Dereks-MacBook-Pro-2.local>
2022-02-10 09:07:39 -0500

    deploy

---
+++ precache-manifest.f8c7789f3a417b25f8d7158c68dbaaee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4280e5c41aea74f8911af091b1102f",
+    "url": "/coins/index.html"
+  },
+  {
+    "revision": "bfa3bb4ef90c00c0fff9",
+    "url": "/coins/js/app.53504c9f.js"
+  },
+  {
+    "revision": "84cd405a59029781fc86",
+    "url": "/coins/js/chunk-vendors.77896d63.js"
+  },
+  {
+    "revision": "5d68433fd06f6ccdfdb013d7537477ed",
+    "url": "/coins/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/coins/robots.txt"
+  }
+]);
